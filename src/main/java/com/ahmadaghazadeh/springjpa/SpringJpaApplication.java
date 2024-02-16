342080b2c86b09651d492cc0ac1c776cbd365487
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2024-02-16 12:24:04 +0330

    Add Mysql and create user

--- src/main/java/com/ahmadaghazadeh/springjpa/SpringJpaApplication.java
+++ src/main/java/com/ahmadaghazadeh/springjpa/SpringJpaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJpaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJpaApplication.class, args);
 	}
